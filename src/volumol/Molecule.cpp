a1dc3ef19bce842bdbedb4b24234005dd01a26c3
GefullteTaubenbrust2 <72752000+GefullteTaubenbrust2@users.noreply.github.com>
2025-04-11 16:37:19 +0200

    Whitespace fix

--- src/volumol/Molecule.cpp
+++ src/volumol/Molecule.cpp
@@ -226,8 +226,8 @@ namespace mol {
 		true , true , true , true , true , true , true , true , true , true , true , true , true , false, false, false, false, false,
 		true , true , true , true , true , true , true , true , true , true , true , true , true , true , true , false, false, false,
 		true , true , true , true , true , true , true , true , true , true , true , true , true , true , true , true ,
-					  true , true , true , true , true , true , true , true , true , true , true , true , true , false, false, false,
+		              true , true , true , true , true , true , true , true , true , true , true , true , true , false, false, false,
 		true , true , true , true , true , true , true , true , true , true , true , true , true , true , true , true ,
-					  true , true , true , true , true , true , true , true , true , true , true , true , true , true , false, false,
+		              true , true , true , true , true , true , true , true , true , true , true , true , true , true , false, false,
 	};
 }
