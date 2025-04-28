4d09c4587caeef4a11ac8c7e4cb999362fb2dc01
Gefüllte Taubenbrust <72752000+GefullteTaubenbrust2@users.noreply.github.com>
2025-04-28 22:32:20 +0200

    Add printProgress()

--- src/logic/ConsoleUtils.cpp
+++ src/logic/ConsoleUtils.cpp
@@ -17,10 +17,35 @@ namespace flo {
 		const int background_colors[16] = {49, 44, 42, 46, 41, 45, 43, 47, 100, 104, 102, 106, 101, 105, 103, 107};
 		std::cout << "\033[" << text_colors[text_color] << 'm';
 		std::cout << "\033[" << background_colors[background_color] << 'm';
-			
 	#endif
 	}
 	void restoreConsoleDefaults() {
 		setConsoleColor(7, 0);
+	}
+
+	float setpoint = 0.f;
+
+	void setConsoleProgress(float progress) {
+		setpoint = progress;
+	}
+
+	void printProgress(float progress) {
+		progress = setpoint + progress * (1.f - setpoint);
+		const int progress_bar_width = 40;
+		int p = progress * progress_bar_width;
+		if (p < 0) p = 0;
+		int q = setpoint * progress_bar_width;
+		if (q < 0) q = 0;
+		char bar[progress_bar_width];
+		for (int i = 0; i < q && i < progress_bar_width; ++i) {
+			bar[i] = '#';
+		}
+		for (int i = q; i < p && i < progress_bar_width; ++i) {
+			bar[i] = '=';
+		}
+		for (int i = p; i < progress_bar_width; ++i) {
+			bar[i] = '-';
+		} 
+		std::cout << "0% " << bar << " 100%" << '\r' << std::flush;
 	}
 }
--- src/logic/ConsoleUtils.h
+++ src/logic/ConsoleUtils.h
@@ -4,4 +4,8 @@ namespace flo {
 	void setConsoleColor(int text_color, int background_color);
 
 	void restoreConsoleDefaults();
+
+	void setConsoleProgress(float progress);
+
+	void printProgress(float progress);
 }
