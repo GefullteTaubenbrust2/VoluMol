b28bd5f0b6757e220da0f76d03579cd04df22fa1
Gefüllte Taubenbrust <72752000+GefullteTaubenbrust2@users.noreply.github.com>
2026-07-24 14:58:14 +0200

    Small fix

--- src/volumol/MolRenderer.cpp
+++ src/volumol/MolRenderer.cpp
@@ -250,7 +250,10 @@ namespace mol::Renderer {
 		b = molecule.getIndex(b);
 		if (a < 0 || a >= molecule.atoms.size() || b < 0 || b >= molecule.atoms.size() || a == b) return;
 		for (int i = 0; i < molecule.bonds.size(); ++i) {
-			if ((molecule.bonds[i].x == a && molecule.bonds[i].y == b) || (molecule.bonds[i].x == b && molecule.bonds[i].y == a)) return;
+			if ((molecule.bonds[i].x == a && molecule.bonds[i].y == b) || (molecule.bonds[i].x == b && molecule.bonds[i].y == a)) {
+				molecule.bonds.erase(molecule.bonds.begin() + i);
+				--i;
+			}
 		}
 		molecule.bonds.push_back(glm::ivec3(a, b, order));
 		update_molecule = true;
